08ada96a5b5a411465cec361d593c3afa707845d
alexlu13 <alexanderlu5000@hotmail.com>
2016-09-15 00:42:27 -0400

    Updated Enemy class a bit

--- src/game/TestEnemy.java
+++ src/game/TestEnemy.java
@@ -1,9 +1,20 @@
 package game;
 
+import static org.lwjgl.opengl.GL11.*;
+
 public class TestEnemy extends Enemy{
 
+	private static final String TEXTURE_FILE = "src\\game\\GameFiles\\Images\\EnemyImages\\TestEnemy\\player.png";
+	
+	private static int imageID;
+	private static boolean loadedTexture = false;
+	
 	public TestEnemy(float x, float y, float maxSpeed, float size) {
 		super(x, y, maxSpeed, size);
+		
+		if(!loadedTexture){
+			imageID = TextureLoader.loadTexture(TEXTURE_FILE);
+		}
 	}
 
 	@Override
@@ -20,20 +31,37 @@ public class TestEnemy extends Enemy{
 
 	@Override
 	public void moveLeft() {
-		// TODO Auto-generated method stub
-		
+		position.addX(-maxSpeed);
 	}
 
 	@Override
 	public void moveRight() {
-		// TODO Auto-generated method stub
+		position.addX(maxSpeed);
 		
 	}
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
 		
+		glBindTexture(GL_TEXTURE_2D, imageID);
+		
+		glBegin(GL_QUADS);{
+			
+			glTexCoord2f(0, 1);
+			glVertex2f(position.getX() - (size / 2), position.getY() - (size / 2));
+			
+			glTexCoord2f(1, 1);
+			glVertex2f(position.getX() + (size / 2), position.getY() - (size / 2));
+			
+			glTexCoord2f(1, 0);
+			glVertex2f(position.getX() + (size / 2), position.getY() + (size / 2));
+			
+			glTexCoord2f(0, 0);
+			glVertex2f(position.getX() - (size / 2), position.getY() + (size / 2));
+			
+		}
+		
+		glEnd();
 	}
 
 	
